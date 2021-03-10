edaab817b094ba621460c6fe7d0a41af18e009c0
Ek <bkwokzj@hotmail.com>
2021-03-10 13:55:19 +1100

    Populated rules page.
    Added rules from #beach-rules

--- components/Dashboard/Rules.js
+++ components/Dashboard/Rules.js
@@ -1,9 +1,165 @@
 import {Box, Text} from 'grommet';
 
+//Template for adding a new rule to this list.
+
+/*
+<Paragraph></Paragraph>
+<br/>
+*/
+
+//Static Page for Server Rules. Only text and CSS here.
 export default function Rules() {
     return (
         <Box>
-            <Text>Rules page - WIP</Text>
+            //Header
+            <Heading alignSelf="center" size="large">Server Rules</Heading>
+            <br/>
+            
+            //Rule 1
+            <Paragraph>
+                <strong>1. Be respectful and don't harass users.</strong> If you feel like there is an issue/feel threatened at any time while in the server, 
+                do not hesitate to DM a mod. We will try and help in any way we can. <br/>
+            </Paragraph>
+            <Paragraph>
+                - Including, but not limited to, racism, hate speech, sexism, ableism, slurs, homophobia, 
+                    personal attacks, name shaming, insults, threats, and other antagonistic behaviors. <br/>
+                - Do not excessively annoy other users. Stop if they ask you to stop. <br/>
+                - Do not harass users to speak about or reveal information they aren’t comfortable sharing. 
+                    Additionally, do not share information a user has told you in confidence. <br/>
+                - Do not harass users in DM. <br/>
+                - Don’t excessively ping users or ping users who have voiced that they do not want to be pinged. 
+                    Pinging a user and then deleting the message is not allowed. <br/>
+                - Keep any personal drama with others out of the public chats; take it to DM. <br/>
+            </Paragraph>
+            <br/>
+
+            //Rule 2
+            <Paragraph>
+                <strong>2. Don't spam messages or emotes outside of spam channels.</strong> <br/>
+            </Paragraph>
+            <Paragraph>
+                - Image posting and emote replies in moderation is fine. However, only posting or excessively posting images and 
+                    emote replies within a short amount of time is not. <br/>
+                - Don’t flood the chat with random messages/emotes/images that have no relevance to any 
+                    conversation at hand or have no intention of starting a conversation. <br/>
+            </Paragraph>
+            <br/>
+            
+            //Rule 3
+            <Paragraph>
+                <strong>3. Don't maliciously troll or shitpost.</strong> <br/>
+            </Paragraph>
+            <Paragraph>
+                -  Do not plan and/or encourage raids against other communities.
+                -  Do not rally people in an attempt to perform activities with malicious intent, such as mass reporting, mobbing, pinging, etc.
+                -  Do not share malicious files or links, malware, and other viruses is prohibited.<br/>
+            </Paragraph>
+            <br/>
+            
+
+            //Rule 4
+            <Paragraph>
+                <strong>4. Keep NSFW talk to a minimum.</strong> Semi-NSFW discussions are allowed, but please keep in mind the other server members before you post. 
+                    However, talks about masturbation, ejaculation, or anything of the sort are forbidden and will result in a warning/ban. 
+                    Please also take note that ahegao images posted by Hololive members are included as part of going against this rule.<br/>
+            </Paragraph>
+            <Paragraph>
+                To clarify the reasoning why we do not allow NSFW in the majority of the channels on our server, 
+                please understand that the staff and the many loyal members of this server work tirelessly to build up and 
+                promote the image of our fan server to provide a safe and comfortable environment for people who are new to Hololive. 
+                We would like to maintain this image and we would appreciate if everyone would do the same.<br/>
+
+                    NSFW images will result in a warning or ban depending on the image and the actions taken during the time of posting.
+                    Regarding NSFW, suggestive content is recommended to not be posted.<br/>
+
+                4.5 To gain access to the server, do the following:<br/>
+                    DM @A-chan the keyword: WASSHOI <br/>
+                    If the bot does not work, please try again in one minute. Message @melon king if it still does not work, 
+                        this may be due to the spike of users, lagging A-chan. Please treat her well. <br/>
+            </Paragraph>
+            <br/>
+
+            //Rule 5
+            <Paragraph>
+                <strong>5. Don't spoil others.</strong> To learn how to spoiler tag: https://support.discordapp.com/hc/en-us/articles/360022320632-Spoiler-Tags-<br/>
+            </Paragraph>
+            <Paragraph>
+                -  Said rule applies to any series, movies, and other media. (Except for VTuber content unless the streamer explicitly stated to keep it a secret until further reveal)
+                -  Please spoiler tag messages/images in channels where spoiler content is not allowed to be freely discussed, generally, you could give mainstream shows a week (7-days) until it's open for discussion. 
+                    When in doubt, ask the present people online if they're fine with discussing it, again, this is to be courteous. 
+                -  Intentionally spoiling another user may be grounds for a ban. <br/>
+            </Paragraph>
+            <br/>
+
+            //Rule 6
+            <Paragraph>
+                <strong>6. Use appropriate usernames and profile pics.</strong> <br/>
+            </Paragraph>
+            <Paragraph>
+                -  Profile pictures must not be offensive, (too) NSFW or contain blatant spoilers of events. 
+                    You will be muted until you change it to something deemed appropriate.<br/>
+                -  Usernames must not be excessively long/shortshort, Offensive, NSFW , Invisible or contain spoilers of events. 
+                    (Please keep usernames at a reasonable length)  Usernames of this nature will be immediately nicknamed by the mod team 
+                    without warning. If you change it back, a mute will be issued. (Including nickname as well) <br/>
+                -  Please do not impersonate another user or a bot. This will make it difficult for people to communicate with you when required. 
+                    If you are caught doing this you will be getting a warning if you have not changed your Discord name when being asked to do so 
+                    and a further mute if you do not comply after multiple requests. <br/>
+            </Paragraph>
+            <br/>
+
+            //Rule 7
+            <Paragraph>
+                <strong>7. Follow chat etiquette.</strong> <br/>
+            </Paragraph>
+            <Paragraph>
+                -  Respect the intended topic of the channel to be discussed. 
+                    Avoid using channels wrongly and if a mod asks you to use an appropriate channel you should move immediately or 
+                    stop the conversation.<br/>
+            </Paragraph>
+            <br/>
+
+            //Rule 8
+            <Paragraph>
+                <strong>8. No NSFL posts or images.</strong> <br/>
+            </Paragraph>
+            <Paragraph>
+                -  NSFL, or Not Safe for Life, posts and images are not allowed in any chat. 
+                    This includes videos, images, other contents showing gore, animal abuse, extreme graphic violence, or anything of the sort. 
+                        Such content goes against Discord's ToS as such, this kind of behavior will not be tolerated and will result in an instant 
+                        second level warning/strike. Please respect your fellow resistance members by being civilized. <br/>
+            </Paragraph>
+            <br/>
+
+            //Rule 9
+            <Paragraph>
+                <strong>9. No NG talk in general chats.</strong> <br/>
+            </Paragraph>
+            <Paragraph>
+                -  What do we mean by NG (no good) talk? Any talk about vtubers' past lives or anything that comes off as stalker-like or 
+                    compromising will not be allowed in any form in general chats, unless the vtubers themselves announce it personally. 
+                    Members found guilty of breaking the rule will be given a strike and will be muted for a considerable amount of time, 
+                    or in some cases, will be issued an instant ban.<br/>
+                -  Topics involving Vtuber-related drama/clout are preferred to be avoided to be talked about on the general chats such as 
+                    #en-general and #🏮-sauna-lounge, for a better moderation on these topics, 
+                    it's preferred to keep them on #▷-hololive-discussion as long as they're Hololive related topics.<br/>
+            </Paragraph>
+            <br/>
+
+            //Rule 10
+            <Paragraph>
+                <strong>10. All text chat is to be free of sensitive topics.</strong> <br/>
+            </Paragraph>
+            <Paragraph>
+                -  Please keep the chats free of sensitive topics such as politics, controversies and religion. 
+                    These topics can tend to spark opinions of different kinds and can become toxic for people so please abstain from 
+                    talking about these topics in general. Those starting conversations with these sensitive topics will be given an instant strike. <br/>
+                -  Historical talks are okay but do avoid sensitive topics that attacks are race or individual as it may incite toxicity. 
+                    Political discussions should be limited as well as political opinions clashing may disturb others who don't want to be involved. 
+                    Religion talks to strictly to be avoided as much as possible. Differing opinions may result in an unwanted clash of 
+                    opinions that may disturb the people at channels. Please avoid unnecessary religious talks, opinionated or otherwise. <br/>
+            </Paragraph>
+            <br/>  
         </Box>
     )
 }
+
